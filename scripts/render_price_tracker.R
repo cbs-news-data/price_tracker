a84a66c36046c44a1179994792c3d04ae1530b53
John Kelly <jkelly3rd@gmail.com>
2024-06-21 11:09:19 -0400

    Update

--- scripts/render_price_tracker.R
+++ scripts/render_price_tracker.R
@@ -1,15 +1,5 @@
 library(rmarkdown)
-
-# Code to build each of the trackers
-# Includes loading pre-processed and stored dfs
-# Grouped by each page to allow for individual or mass processing
-
-# MURDERS
-# Load RDS
-murders_district <- readRDS("scripts/rds/murders_district.rds")
-murders_city <- readRDS("scripts/rds/murders_city.rds")
-asofdate <- readRDS("scripts/rds/asofdate.rds")
 # Render page
-rmarkdown::render('scripts/Philadelphia_Safety_Tracker.Rmd', 
+rmarkdown::render('scripts/cbs_news_price_tracker.Rmd', 
                   output_dir = "docs",
-                  output_file = 'Philadelphia_Safety_Tracker.html')
+                  output_file = 'cbs_news_price_tracker.html')
