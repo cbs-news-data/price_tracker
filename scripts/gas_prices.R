d3c1a10a209b77e82288054b116b2ee48e002d5a
John Kelly <jkelly3rd@gmail.com>
2024-08-07 14:15:15 -0400

    Gas code

--- scripts/gas_prices.R
+++ scripts/gas_prices.R
@@ -23,6 +23,8 @@ colnames(gas_prices) <- sub(" Regular.*", "", colnames(gas_prices))
 colnames(gas_prices) <- sub("Weekly ", "", colnames(gas_prices))
 # Repeat to keep only the string before " ("
 colnames(gas_prices) <- sub(" \\(.*", "", colnames(gas_prices))
+# Repeat to keep only the remaining string before ", "
+colnames(gas_prices) <- sub(", .*", "", colnames(gas_prices))
 # Rename the first column to "date"
 colnames(gas_prices)[1] <- "date"
 # convert the date to a date
@@ -49,7 +51,7 @@ gas_prices %>% select(1:2,20:29) %>% write_csv("data/gas_prices_cities.csv")
 
 # Assuming 'oil_prices' is a data frame with a 'date' column
 # Convert the date to the desired format
-gas_date <- format(max(gas_prices$date), "%B %d")
+gas_date <- format(max(gas_prices$date), "%B %-d")
 
 # Create the description string
 gas_description <- paste("The price per gallon reported by the Energy Information Administration as of ", gas_date, ". Hover anywhere on the chart to see the price for any specific week. The red line shows the U.S. average by comparison.",sep="")
