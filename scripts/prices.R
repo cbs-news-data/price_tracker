06ddc26ddff085138c4f98a97dd6cf50f459eacf
John Kelly <jkelly3rd@gmail.com>
2024-09-10 19:27:33 -0400

    Update steak price

--- scripts/prices.R
+++ scripts/prices.R
@@ -13,12 +13,12 @@ key <- Sys.getenv("BLS_API_KEY")
 
 # Define series IDs and corresponding item names
 series_ids <- c('APU0000709112', 'APU0000704111', 'APU0000711211', 'APU0000703112', 'APU0000FF1101', 
-                'APU0000702111', 'APU0000708111', 'APU0000702421', 'APU000074714', 'APU0000712112', 
+                'APU0000702111', 'APU0000708111', 'APU0000702421', 'APU0000703511', 'APU0000712112', 
                 'APU0000717311', 'APU000072620', 'APU0000FN1101', 'APU0000710212', 'APU000072610', 
                 'APU0000713111', 'APU0000FJ4101', 'APU0000712311','APU0000720311','APU0000711415',
                 'APU0000701312', 'APU0000710411', 'APU0000FS1101', 'APU0000701322')
 item_names <- c("Milk (half gallon)", "Bacon (pound)", "Bananas (pound)", "Ground Beef (pound)", 
-                "Chicken Breast (pound)", "Loaf of Bread", "Dozen eggs", "Cookies", "Regular Unleaded Gasoline (gallon)", 
+                "Chicken Breast (pound)", "Loaf of Bread", "Dozen eggs", "Cookies", "Steak (pound)", 
                 "Potatoes (pound)", "Coffee (pound)", "Utility gas", "2-liter soft drink", 
                 "Cheddar Cheese (pound)", "Electricity (kw hour)", "Frozen orange juice", 
                 "Yogurt (8 ounces)", "Tomatoes (pound)", "Table Wine", "Strawberries (pint)", "Rice (pound)",
